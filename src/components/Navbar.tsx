14eb4c4dc30e97400bb0d6c2b079ef6d435cf3be
Best Tosan <besttosan@gmail.com>
2025-11-11 09:57:53 +0100

    about

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,9 +38,9 @@ export default function Navbar() {
         {/* === DESKTOP MENU === */}
         <ul className="hidden md:flex items-center space-x-10 text-sm font-medium">
           <li>
-            <a href="#about" className="hover:text-yellow-500 transition tracking-widest">
+            <Link href="/about" className="hover:text-yellow-500 transition tracking-widest">
               About Us
-            </a>
+            </Link>
           </li>
           <li>
             <Link href="/models" className="hover:text-yellow-500 transition tracking-widest">
